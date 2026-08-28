8e94b014e2bdabf53a5497b161f053a09685a9f7
Tom Aronson <tmaronson@gmail.com>
2026-08-27 20:23:54 -0400

    Small change to one SQL file again.

--- sql/sql.shelters_state.sql
+++ sql/sql.shelters_state.sql
@@ -1,5 +1,3 @@
 SELECT name, address, email, phone, ST_Y(geom::geometry), ST_X(geom::geometry) 
  FROM shelters 
- WHERE TRIM(UPPER(state_code)) = TRIM(UPPER(%s))
- 
- --WHERE state_code = %s; 
+ WHERE state_code = %s;
